eb9c04b7bfbcdae73a44fce0f93401ff89d27b0c
Ingnacio Cassi <nacho.cassi97@gmail.com>
2022-07-24 22:57:18 -0300

    Added functionalities for performing a basic auth HTTP GET petition to a Subversion server, and parsing the response HTML to a repo_list.

--- svntogit.py
+++ svntogit.py
@@ -1,37 +1,49 @@
-import base64
 import urllib
 from urllib import request
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
 
 
-def read_subversion_server_data():
+def prompt_subversion_server_credentials():
     subversion_url = input("[*] Please enter the URL address for target Subversion server: ")
     subversion_username = input("[*] Please enter the username for authenticating in Subversion server: ")
     subversion_password = input("[*] Please enter the password authenticating in Subversion server: ")
     return subversion_url, subversion_username, subversion_password
 
 
-def access_subversion_server(subversion_url, subversion_username, subversion_password):
-    request_svn = urllib.request.Request(subversion_url)
-    padded_auth = set_base64_padding(subversion_username + ":" + subversion_password)
-    b64auth = base64.standard_b64decode(padded_auth)
-    request_svn.add_header("Authorization", "Basic %s" % b64auth)
-    request_svn.full_url = "http://sources.aa2000.com.ar/subversion/"
-    response = urllib.request.urlopen(request_svn)
-    print(response)
+def get_subversion_server_response(subversion_url, subversion_username, subversion_password):
+    url_opener, svn_request = get_url_opener_and_request(subversion_url, subversion_username, subversion_password)
+    try:
+        result = url_opener.open(svn_request)
+        response = result.read()
+        return response
+    except HTTPError:
+        print("[X] The username, password or URL entered are incorrect please try again...\n")
+        prompt_subversion_server_credentials()
 
 
-def set_base64_padding(string):
-    outer_part = len(string) % 4
-    if outer_part > 0:
-        padding_amount = 4 - outer_part
-        string = string + padding_amount * "="
-    return string
+def get_url_opener_and_request(subversion_url, subversion_username, subversion_password):
+    password_manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    password_manager.add_password(None, subversion_url, subversion_username, subversion_password)
+    auth_handler = urllib.request.HTTPBasicAuthHandler(password_manager)
+    url_opener = urllib.request.build_opener(auth_handler)
+    urllib.request.install_opener(url_opener)
+    svn_request = urllib.request.Request(subversion_url)
+    return url_opener, svn_request
+
+
+def parse_response_to_repo_list(response):
+    soup = BeautifulSoup(response, features="html.parser")
+    return soup.find('ul')
 
 
 def __main__():
     # TODO: Read and validate Subversion URL from user input.
-    subversion_url, subversion_username, subversion_password = read_subversion_server_data()
-    access_subversion_server(subversion_url, subversion_username, subversion_password)
+    # TODO: Handle wrong credentials exception.
+    # TODO: Implement html response processing to obtain the list of repository names.
+    subversion_url, subversion_username, subversion_password = prompt_subversion_server_credentials()
+    response = get_subversion_server_response(subversion_url, subversion_username, subversion_password)
+    repo_list = parse_response_to_repo_list(response)
 
 
 if __name__ == "__main__":
